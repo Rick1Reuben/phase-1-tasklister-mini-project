deb355c0cf75a9858afd20f37487b631f91a036c
reuben <rickr6129@gmail.com>
2024-04-12 20:19:42 +0300

    completed a lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // Myy code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    const taskInput = document.getElementById("new-task-description");
+    const taskText = taskInput.value;
+
+    const newTask = document.createElement("li");
+    newTask.textContent = taskText;
+
+    taskList.appendChild(newTask);
+
+    taskInput.value = "";
+  });
 });
